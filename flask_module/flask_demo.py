536436c56c323636a51bce2ed3c41f0b4534c6e8
Asdfrew!23 <ChrisChen2050@outlook.com>
2022-03-13 13:38:48 -0400

    .

--- flask_module/flask_demo.py
+++ flask_module/flask_demo.py
@@ -3,7 +3,7 @@
 from flask import Flask,jsonify,request
 app = Flask(__name__)
 
-from flask_module.string_to_table import string_to_table
+from flask_module.string_to_table import string_to_table, data_module
 import requests
 
 @app.route('/')
@@ -11,22 +11,48 @@ def hello_flask():
    return 'Flask'
 
 
-@app.route('/hello')
-def hello_world():
-   return 'flask: [Hello]'
+@app.route('/hello_text')
+def hello_text():
+   return 'flask-text: [Hello]'
 
 
-@app.route('/json_test')
+@app.route('/hello_json')
 def hello_json():
    return jsonify(dict(name='jname', value=['json_test']))
 
 
-@app.route('/table/<table_string>')
-def get_product(table_string):
+@app.route('/table_string/<table_string>')
+def string_to_table(table_string):
    table_info = string_to_table(table_string)
    keys = table_info.keys()
    print(keys)
    return jsonify(table_info)
+
+@app.route('/mysql')
+def list_db():
+   return jsonify(data_module.list_db())
+
+@app.route('/mysql/<db_name>')
+def list_table(db_name):
+   return jsonify(data_module.list_table(db_name))
+
+@app.route('/mysql/<db_name>/<tb_name>')
+def get_table(db_name, tb_name):
+   table = data_module.get_table(db_name, tb_name)
+   print('table_info:', table)
+   cols = [dict(
+      name=col['name'],
+      cname=col['cname'] if len(col['cname'])<10 else col['cname'][:10]+' ...',
+      type=col['type']
+      )
+      for col in table['cols']
+   ]
+   return jsonify(dict(
+      tb_name=table['table_name'],
+      tb_cname = table['table_cn'],
+      cols=cols,
+      samples=table['data']
+   ))
 
 
 # @app.route('/next')
--- flask_module/string_to_table.py
+++ flask_module/string_to_table.py
@@ -4,18 +4,44 @@
 # pandas.set_option('max_columns', None)
 import pickle
 
+class Data_module:
+    def get_module(self):
+        module = None
+        with open('big_file/data_module.pkl', 'rb') as f:
+            module = pickle.load(f)
+        return module    
+    def list_db(self):
+        module = self.get_module()
+        return list(module.keys())
+    def list_table(self, db_name):
+        module = self.get_module()
+        table_list = module[db_name]
+        return [tb['table_name'] for tb in table_list]
+    def get_table(self, db_name, table_name):
+        module = self.get_module()
+        table_list = module[db_name]
+        for table in table_list:
+            if table['table_name'] == table_name:
+                return table
+        return None
+        
+
+data_module = Data_module()
+
+
 def string_to_table(py_string = 'mysql.db143.sq_fin_procfnew_tmp'):
     method, db_name, table_name = py_string.split('.')
-    module = None
-    with open('big_file/data_module.pkl', 'rb') as f:
-        module = pickle.load(f)
+    return data_module.get_table(db_name, table_name)
+    # # module = None
+    # # with open('big_file/data_module.pkl', 'rb') as f:
+    # #     module = pickle.load(f)
 
-    table_list = module[db_name]
-    table = [table for table in table_list if table['table_name']==table_name][0]
-    # print('find table:')
-    # print(table)
-    # print(table.keys())
-    return table
+    # table_list = module[db_name]
+    # table = [table for table in table_list if table['table_name']==table_name][0]
+    # # print('find table:')
+    # # print(table)
+    # # print(table.keys())
+    # return table
 
 
 
